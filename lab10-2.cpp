2fb0561f01ee572ebbf282fc77442a84ff368090
ashtonwood1 <ashtonhughwood@outlook.com>
2024-12-06 19:23:24 +0000

    lab 10-2

--- lab10-2.cpp
+++ lab10-2.cpp
@@ -1,12 +1,60 @@
 //Lab10-2.cpp - displays total owed
-//Created/revised by <your name> on <current date>
+//Created/revised by <Ashton Wood> on <current date>
 
 #include <iostream>
 #include <iomanip>
 using namespace std;
-
+void displayOptions();
+void getRegular(int windows, double price, double &total);
+void getBoGo(int windows, double price, double &total);
 int main()
 {
-
+    int option = 0;
+    int numOrdered = 0;
+    double winPrice = 0.0;
+    double totalOwed = 0.0;
+     
+    cout << fixed << setprecision(2);
+     
+    displayOptions();
+    cout << "Pricing option? ";
+    cin >> option;
+     
+    if (option == 1 || option == 2)
+    {
+        cout << "Number of windows: ";
+        cin >> numOrdered;
+        cout << "Price per window: ";
+        cin >> winPrice;
+     
+        if (option == 1)
+            getRegular(numOrdered, winPrice, totalOwed);
+        else
+            getBoGo(numOrdered, winPrice, totalOwed);
+             
+     
+        cout << "Total owed $" << totalOwed << endl << endl;
+         }
+    else
+        cout << "Invalid option" << endl;
+        
+     
     return 0;
-} //end of main function
+      }   //end of main function
+    
+// function definitions    
+void displayOptions()
+{
+    cout << "Pricing options:" << endl;
+    cout << "1   Regular pricing" << endl;
+    cout << "2   BOGO pricing" << endl;
+}  
+void getRegular(int windows, double price, double &total)
+{
+    total = windows * price;
+}  
+ 
+void getBoGo(int windows, double price, double &total)
+{
+    total = (windows / 2 + windows % 2) * price;
+}  
